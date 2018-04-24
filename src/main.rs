58e13018d5b13b434e4900d3fd12690c5862c330
Rivalo <rivalo@home.nl>
2018-04-24 16:06:37 +0200

    Rocket Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-fn main() {
-    let x = 5;
-    println!("Print the number: {}", x);
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
 
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
 }
