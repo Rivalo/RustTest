8bec2f395dd64ca0e44512555322bfeb8ef18784
Rivalo <rivalo@home.nl>
2018-04-24 22:03:43 +0200

    formatting

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,20 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
-#[macro_use] extern crate rocket_contrib;
-#[macro_use] extern crate serde_derive;
 
-use rocket_contrib::{Json, Value};
-use rocket::State;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+
 use rocket::response::NamedFile;
+use rocket::State;
+use rocket_contrib::{Json, Value};
 
 use std::collections::HashMap;
-use std::sync::Mutex;
 use std::io;
 use std::path::{Path, PathBuf};
-
+use std::sync::Mutex;
 
 type LedHashMap = HashMap<usize, LED>;
 type LedMap = Mutex<LedHashMap>;
@@ -22,7 +24,7 @@ type LedMap = Mutex<LedHashMap>;
 struct LED {
     r: u8,
     g: u8,
-    b: u8
+    b: u8,
 }
 
 #[get("/")]
@@ -36,13 +38,13 @@ fn files(file: PathBuf) -> Option<NamedFile> {
 }
 
 #[get("/led/<id>", format = "application/json")]
-fn getled(id: usize, map: State<LedMap>) -> Option<Json<LED>>{
+fn getled(id: usize, map: State<LedMap>) -> Option<Json<LED>> {
     let hashmap = map.lock().unwrap();
     hashmap.get(&id).map(|leds| {
         Json(LED {
             r: leds.clone().r,
             g: leds.clone().g,
-            b: leds.clone().b
+            b: leds.clone().b,
         })
     })
 }
@@ -58,42 +60,33 @@ fn setled(id: usize, message: Json<LED>, map: State<LedMap>) -> Option<Json<Valu
     } else {
         None
     }
-
 }
 
 #[get("/led")]
-fn amountled(map: State<LedMap>) -> Option<Json<Value>>{
+fn amountled(map: State<LedMap>) -> Option<Json<Value>> {
     let hashmap = map.lock().unwrap();
     let length = hashmap.len();
     Some(Json(json!({ "length": length })))
-
-
 }
 
-fn led_update_handler(id: usize, led: &LED){
+fn led_update_handler(id: usize, led: &LED) {
     println!("LED {} HAS BEEN UPDATED", id);
     println!("{:?}", led)
 }
 
 fn main() {
     let mut leds = LedHashMap::new();
-    let led1 = LED{
-        r: 1,
-        g: 2,
-        b: 3
-    };
+    let led1 = LED { r: 1, g: 2, b: 3 };
 
-    let led2 = LED{
-        r: 4,
-        g: 5,
-        b: 6
-    };
+    let led2 = LED { r: 4, g: 5, b: 6 };
 
     leds.insert(1, led1);
     leds.insert(2, led2);
 
     let led_map = LedMap::new(leds);
 
-
-    rocket::ignite().mount("/", routes![index, getled, setled, amountled, files]).manage(led_map).launch();
+    rocket::ignite()
+        .mount("/", routes![index, getled, setled, amountled, files])
+        .manage(led_map)
+        .launch();
 }
